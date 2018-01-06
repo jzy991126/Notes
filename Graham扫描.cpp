187ea9cc006b1917622edc41d41358492b6102b6
Enzymii EcoRI <rtyrty1018@163.com>
2018-01-06 22:00:41 +0800

    A more reliable style

--- Graham扫描.cpp
+++ Graham扫描.cpp
@@ -10,8 +10,7 @@ int dcmp(const double &a){
 struct point{
 	double x,y;
 	point(const double &X=0,const double &Y=0):x(X),y(Y){}
-}p[N];
-int stk[N],tp,mi;
+}p[N],stk[N];int tp,mi;
 point operator -(const point &a,const point &b){
 	return point(a.x-b.x,a.y-b.y);
 }
@@ -24,34 +23,34 @@ double operator *(const point &a,const point &b){
 double len(const point &a){
 	return sqrt(a^a);
 }
-bool cmpa(const point &a,const point &b){
-	point X=point(1,0),A=a-p[0],B=b-p[0];
-	double coa=(A^X)/len(A),cob=(B^X)/len(B);
-	return dcmp(coa-cob)>0;
-} //°´¼Ğ½ÇÅÅĞò(µã»ı°æ) 
+//bool cmpa(const point &a,const point &b){
+//	point X=point(1,0),A=a-p[0],B=b-p[0];
+//	double coa=(A^X)/len(A),cob=(B^X)/len(B);
+//	return dcmp(coa-cob)>0;
+//} //æŒ‰å¤¹è§’æ’åº(ç‚¹ç§¯ç‰ˆ)
 bool cmpa(const point &a,const point &b){
   	point A=a-p[0],B=b-p[0];
   	return dcmp(A*B)>0;
-} //²æ»ı°æ 
+} //å‰ç§¯ç‰ˆ 
 void grahamScan(point* p,int n){
 	std::sort(p+1,p+n,cmpa);
-	stk[++tp]=0; stk[++tp]=1;
+	stk[++tp]=p[0]; stk[++tp]=p[1];
 	for(int i=2;i<n;++i){
-		while(dcmp((p[i]-p[stk[tp]])*(p[stk[tp]]-p[stk[tp-1]]))>0) --tp; //Ë³Ê±Õë¾ÍÍËÕ» 
-		stk[++tp]=i; //½øÕ»
+		while(dcmp((p[i]-stk[tp])*(stk[tp]-stk[tp-1]))>0) --tp; //é¡ºæ—¶é’ˆå°±é€€æ ˆ 
+		stk[++tp]=p[i]; //è¿›æ ˆ
 	}
 }
 int main(){
 	int n; scanf("%d",&n); double my=1e9,ans=0;
 	for(int i=0;i<n;++i){
 		scanf("%lf%lf",&p[i].x,&p[i].y);
-		//ÕÒµ½pµã.. ±¾¸Ã·Åµ½º¯ÊıÀï´¦Àí µ«ÊÇÎªÁËÉÙÒ»ÖØÑ­»·¾ÍÔÚÕâÀï´¦ÀíÁË 
+		//æ‰¾åˆ°pç‚¹.. æœ¬è¯¥æ”¾åˆ°å‡½æ•°é‡Œå¤„ç† ä½†æ˜¯ä¸ºäº†å°‘ä¸€é‡å¾ªç¯å°±åœ¨è¿™é‡Œå¤„ç†äº† 
 		if(dcmp(p[i].y-my)<0||
 		(!dcmp(p[i].y-my)&&dcmp(p[i].x-p[mi].x)<0))
 			my=p[i].y,mi=i;	 
 	} std::swap(p[0],p[mi]);
 	grahamScan(p,n);
 	for(int i=1;i<tp;++i)
-		ans+=len(p[stk[i]]-p[stk[i+1]]);
-	printf("%.2lf",ans+len(p[stk[tp]]-p[0]));
+		ans+=len(stk[i]-stk[i+1]);
+	printf("%.2lf",ans+len(stk[tp]-p[0]));
 }
